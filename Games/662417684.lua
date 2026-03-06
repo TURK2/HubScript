18c853ba87acb750b994a49149f11f0d053ecd1c
TURK2 <137105402+TURK2@users.noreply.github.com>
2026-03-06 19:44:12 +0700

    Update 662417684.lua

--- Games/662417684.lua
+++ Games/662417684.lua
@@ -1,1 +1,70 @@
-g
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Window = Rayfield:CreateWindow({
+   Name = "LuckyBlock Hub",
+   LoadingTitle = "LuckyBlock Hub",
+   LoadingSubtitle = "YT: TURKXSRCIPT",
+   ConfigurationSaving = {
+      Enabled = false
+   }
+})
+
+-- Main Tab
+local MainTab = Window:CreateTab("Main", 4483362458)
+
+MainTab:CreateButton({
+   Name = "Spawn LuckyBlock",
+   Callback = function()
+      ReplicatedStorage.SpawnLuckyBlock:FireServer()
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Spawn Super Block",
+   Callback = function()
+      ReplicatedStorage.SpawnSuperBlock:FireServer()
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Spawn Diamond Block",
+   Callback = function()
+      ReplicatedStorage.SpawnDiamondBlock:FireServer()
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Spawn Rainbow Block",
+   Callback = function()
+      ReplicatedStorage.SpawnRainbowBlock:FireServer()
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Spawn Galaxy Block",
+   Callback = function()
+      ReplicatedStorage.SpawnGalaxyBlock:FireServer()
+   end,
+})
+
+-- About Tab
+local AboutTab = Window:CreateTab("About", 4483362458)
+
+AboutTab:CreateParagraph({
+   Title = "Credits",
+   Content = "YouTube: TURKXSRCIPT"
+})
+
+AboutTab:CreateButton({
+   Name = "Copy Discord Link",
+   Callback = function()
+      setclipboard("https://discord.gg/yourlink")
+      Rayfield:Notify({
+         Title = "Copied!",
+         Content = "Discord link copied to clipboard",
+         Duration = 4,
+      })
+   end,
+})
