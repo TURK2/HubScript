35a07b790034d2120f7d74ee913418e5fe596864
TURK2 <137105402+TURK2@users.noreply.github.com>
2026-03-09 12:33:06 +0700

    Update 106772177198260.lua

--- Games/106772177198260.lua
+++ Games/106772177198260.lua
@@ -14,24 +14,27 @@ local AutoCatch = false
 local AutoBuyRod = false
 local AutoRebirth = false
 
+-- speed
+local CollectSpeed = 0.2
+
 local DeleteSetting = {
-        Uncommon = false,
-        Rare = false,
-        Epic = false,
-        Legendary = false,
-        Mythic = false,
-        Godly = false,
-        Divine = false,
-        Secret = false
+    Uncommon = false,
+    Rare = false,
+    Epic = false,
+    Legendary = false,
+    Mythic = false,
+    Godly = false,
+    Divine = false,
+    Secret = false
 }
 
 -------------------------------------------------
 
 local Window = Rayfield:CreateWindow({
-        Name = "Fishing Hub",
-        LoadingTitle = "Auto Farm",
-        LoadingSubtitle = "Script",
-        ConfigurationSaving = {Enabled = false}
+    Name = "Fishing Hub",
+    LoadingTitle = "Auto Farm",
+    LoadingSubtitle = "Script",
+    ConfigurationSaving = {Enabled = false}
 })
 
 local TabEN = Window:CreateTab("Main EN",4483362458)
@@ -42,28 +45,40 @@ local TabTH = Window:CreateTab("Main TH",4483362458)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoReel then
-                        ReplicatedStorage.RemoteHandler.Fishing:FireServer("Caught",2)
-                end
-                task.wait(0.05)
+    while true do
+        if AutoReel then
+            ReplicatedStorage.RemoteHandler.Fishing:FireServer("Caught",2)
         end
+        task.wait(0.05)
+    end
 end)
 
 -------------------------------------------------
--- Auto Collect
+-- Auto Collect (1 -> 2 -> 3 -> ...)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoCollect then
-                        for i = 1,30 do
-                                ReplicatedStorage.RemoteHandler.Collect:FireServer("Plot"..i)
-                                task.wait(0.05)
-                        end
-                end
-                task.wait(0.2)
+
+    local PlotIndex = 1
+
+    while true do
+
+        if AutoCollect then
+
+            ReplicatedStorage.RemoteHandler.Collect:FireServer("Plot"..PlotIndex)
+
+            PlotIndex = PlotIndex + 1
+
+            if PlotIndex > 30 then
+                PlotIndex = 1
+            end
+
         end
+
+        task.wait(CollectSpeed)
+
+    end
+
 end)
 
 -------------------------------------------------
@@ -71,12 +86,12 @@ end)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoPower then
-                        ReplicatedStorage.RemoteHandler.Upgrade:FireServer("power1")
-                end
-                task.wait(0.1)
+    while true do
+        if AutoPower then
+            ReplicatedStorage.RemoteHandler.Upgrade:FireServer("power1")
         end
+        task.wait(0.1)
+    end
 end)
 
 -------------------------------------------------
@@ -84,12 +99,12 @@ end)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoCatch then
-                        ReplicatedStorage.RemoteHandler.Upgrade:FireServer("catch1")
-                end
-                task.wait(0.1)
+    while true do
+        if AutoCatch then
+            ReplicatedStorage.RemoteHandler.Upgrade:FireServer("catch1")
         end
+        task.wait(0.1)
+    end
 end)
 
 -------------------------------------------------
@@ -97,15 +112,15 @@ end)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoRodMutation then
-                        for i = 1,7 do
-                                ReplicatedStorage.RemoteHandler.RodMutation:FireServer("Buy","RodMutation"..i)
-                                task.wait(0.5)
-                        end
-                end
-                task.wait(2)
+    while true do
+        if AutoRodMutation then
+            for i = 1,7 do
+                ReplicatedStorage.RemoteHandler.RodMutation:FireServer("Buy","RodMutation"..i)
+                task.wait(0.5)
+            end
         end
+        task.wait(2)
+    end
 end)
 
 -------------------------------------------------
@@ -113,15 +128,15 @@ end)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoBuyRod then
-                        for i = 1,17 do
-                                ReplicatedStorage.RemoteHandler.FishingRod:FireServer("Buy","FishingRod"..i)
-                                task.wait(0.5)
-                        end
-                end
-                task.wait(3)
+    while true do
+        if AutoBuyRod then
+            for i = 1,17 do
+                ReplicatedStorage.RemoteHandler.FishingRod:FireServer("Buy","FishingRod"..i)
+                task.wait(0.5)
+            end
         end
+        task.wait(3)
+    end
 end)
 
 -------------------------------------------------
@@ -129,12 +144,12 @@ end)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoRebirth then
-                        ReplicatedStorage.RemoteHandler.Rebirth:FireServer()
-                end
-                task.wait(1)
+    while true do
+        if AutoRebirth then
+            ReplicatedStorage.RemoteHandler.Rebirth:FireServer()
         end
+        task.wait(1)
+    end
 end)
 
 -------------------------------------------------
@@ -142,12 +157,12 @@ end)
 -------------------------------------------------
 
 task.spawn(function()
-        while true do
-                if AutoSell then
-                        ReplicatedStorage.RemoteHandler.SellMultiple:FireServer(DeleteSetting)
-                end
-                task.wait(0.5)
+    while true do
+        if AutoSell then
+            ReplicatedStorage.RemoteHandler.SellMultiple:FireServer(DeleteSetting)
         end
+        task.wait(0.5)
+    end
 end)
 
 -------------------------------------------------
@@ -155,151 +170,171 @@ end)
 -------------------------------------------------
 
 TabEN:CreateDropdown({
-        Name = "Fish To Delete",
-        Options = {"Uncommon","Rare","Epic","Legendary","Mythic","Godly","Divine","Secret"},
-        CurrentOption = {},
-        MultipleOptions = true,
-        Callback = function(options)
+    Name = "Fish To Delete",
+    Options = {"Uncommon","Rare","Epic","Legendary","Mythic","Godly","Divine","Secret"},
+    CurrentOption = {},
+    MultipleOptions = true,
+    Callback = function(options)
 
-                for k in pairs(DeleteSetting) do
-                        DeleteSetting[k] = false
-                end
-
-                for _,v in pairs(options) do
-                        DeleteSetting[v] = true
-                end
-
+        for k in pairs(DeleteSetting) do
+            DeleteSetting[k] = false
         end
+
+        for _,v in pairs(options) do
+            DeleteSetting[v] = true
+        end
+
+    end
+})
+
+TabEN:CreateSlider({
+    Name = "Collect Speed",
+    Range = {0.05,1},
+    Increment = 0.05,
+    CurrentValue = 0.2,
+    Callback = function(Value)
+        CollectSpeed = Value
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Sell Selected Fish",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoSell = v
-        end
+    Name = "Auto Sell Selected Fish",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoSell = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Reel",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoReel = v
-        end
+    Name = "Auto Reel",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoReel = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Collect",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoCollect = v
-        end
+    Name = "Auto Collect",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoCollect = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Buy Power",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoPower = v
-        end
+    Name = "Auto Buy Power",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoPower = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Buy Catch",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoCatch = v
-        end
+    Name = "Auto Buy Catch",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoCatch = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Buy RodMutation",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoRodMutation = v
-        end
+    Name = "Auto Buy RodMutation",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoRodMutation = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Buy FishingRod",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoBuyRod = v
-        end
+    Name = "Auto Buy FishingRod",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoBuyRod = v
+    end
 })
 
 TabEN:CreateToggle({
-        Name = "Auto Rebirth",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoRebirth = v
-        end
+    Name = "Auto Rebirth",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoRebirth = v
+    end
 })
 
 -------------------------------------------------
 -- TH UI
 -------------------------------------------------
 
-TabTH:CreateToggle({
-        Name = "รีลปลาอัตโนมัติ",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoReel = v
-        end
+TabTH:CreateSlider({
+    Name = "ความเร็วเก็บเงิน",
+    Range = {0.05,1},
+    Increment = 0.05,
+    CurrentValue = 0.2,
+    Callback = function(Value)
+        CollectSpeed = Value
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "เก็บเงิน ออโต้",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoCollect = v
-        end
+    Name = "รีลปลาอัตโนมัติ",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoReel = v
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "ซื้อ Power อัตโนมัติ",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoPower = v
-        end
+    Name = "เก็บเงิน ออโต้",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoCollect = v
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "ซื้อ Catch อัตโนมัติ",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoCatch = v
-        end
+    Name = "ซื้อ Power อัตโนมัติ",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoPower = v
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "ซื้อ Mutation เบ็ด",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoRodMutation = v
-        end
+    Name = "ซื้อ Catch อัตโนมัติ",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoCatch = v
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "ซื้อเบ็ด ออโต้",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoBuyRod = v
-        end
+    Name = "ซื้อ Mutation เบ็ด",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoRodMutation = v
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "รีเบิร์ดอัตโนมัติ",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoRebirth = v
-        end
+    Name = "ซื้อเบ็ด ออโต้",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoBuyRod = v
+    end
 })
 
 TabTH:CreateToggle({
-        Name = "ขายปลาอัตโนมัติ",
-        CurrentValue = false,
-        Callback = function(v)
-                AutoSell = v
-        end
+    Name = "รีเบิร์ดอัตโนมัติ",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoRebirth = v
+    end
+})
+
+TabTH:CreateToggle({
+    Name = "ขายปลาอัตโนมัติ",
+    CurrentValue = false,
+    Callback = function(v)
+        AutoSell = v
+    end
 })
