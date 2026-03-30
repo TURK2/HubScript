d3940c8d98b9ecf8a14876a35f1fdaa2e48f3ca5
TURK2 <137105402+TURK2@users.noreply.github.com>
2026-03-30 13:48:25 +0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,47 @@
+repeat task.wait() until game:IsLoaded()
+
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+local TeleportService = game:GetService("TeleportService")
+local plr = game.Players.LocalPlayer
+
+-- 🔗 API list ไฟล์ใน GitHub
+local API = "https://api.github.com/repos/TURK2/HubScript/contents/Games"
+
+local Window = Rayfield:CreateWindow({
+    Name = "Select Game",
+    ConfigurationSaving = {Enabled = false}
+})
+
+local Tab = Window:CreateTab("Maps",4483362458)
+
+local selected = false
+
+-- ⚡ โหลดรายชื่อแมพ
+task.spawn(function()
+    local ok,res = pcall(game.HttpGet,game,API,true)
+    if not ok then return end
+
+    local data = game:GetService("HttpService"):JSONDecode(res)
+
+    for _,file in pairs(data) do
+        if file.name:find(".lua") then
+            local id = file.name:gsub(".lua","")
+
+            Tab:CreateButton({
+                Name = "Join Map: "..id,
+                Callback = function()
+                    selected = true
+                    TeleportService:Teleport(tonumber(id), plr)
+                end
+            })
+        end
+    end
+end)
+
+-- ⏱️ เตะถ้าไม่เลือก
+task.spawn(function()
+    task.wait(10)
+    if not selected then
+        plr:Kick("Please select a map")
+    end
+end)
